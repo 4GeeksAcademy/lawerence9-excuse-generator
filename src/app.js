35832ae5040e17d5cffc2c39f8eee48abb6fd78f
Lawerence9 <82092265+Lawerence9@users.noreply.github.com>
2024-11-20 19:34:31 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,34 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let entity = ["The tiger", "My sea lion", "His cheetah", "My lion"];
+  let reason = ["stomped", "pissed", "escaped", "broke"];
+  let objectTampered = [
+    "my homework",
+    "my friend's house",
+    "the neighbour's garden",
+    "the king's courtyard"
+  ];
+  let excuse = [
+    "before the class",
+    "in the bathroom",
+    "when I was at the gym",
+    "during my lunch",
+    "while I was playing SMTVV"
+  ];
+  let randomEntity = Math.floor(Math.random() * entity.length);
+  let randomObjectTampered = Math.floor(Math.random() * objectTampered.length);
+  let randomReason = Math.floor(Math.random() * reason.length);
+  let randomExcuse = Math.floor(Math.random() * excuse.length);
+
+  document.querySelector("#theExcuse").innerHTML =
+    entity[randomEntity] +
+    " " +
+    reason[randomReason] +
+    " " +
+    objectTampered[randomObjectTampered] +
+    " " +
+    excuse[randomExcuse];
 };
